4a6a3adf0a161c2d1b1dc3da509624a623706707
Jefferson Chavez Diaz <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-01 18:51:37 -0500

    New content: templates union types, types alias, objects, functions, arrow functions, arrays...

--- 1.ts
+++ 1.ts
@@ -1,4 +1,6 @@
-//Tipos de datos básicos
+// COMO EJECUTAR TYPESSCRIPT DESDE LA LINEA DE COMANDO -> https://rootstack.com/es/blog/nodejs-tsnode
+
+//Types of data
 const number = 1
 let n: number = 2
 
@@ -31,15 +33,15 @@ cadenaDeTexto.toLocaleLowerCase()
 // ❌ cadenaDeTexto.propiedadInexistente
 
 //Any
-let obj: any = {
-  x: 0,
-}
+// let obj: any = {
+//   x: 0,
+// }
 
-obj.foo()
-obj()
-obj.bar = 100
-obj = 'hello'
-const N: number = obj
+// obj.foo()
+// obj()
+// obj.bar = 10
+// obj = 'hello'0
+// const N: number = obj
 
 // Functions
 function saludar(firstName: string) {
@@ -49,23 +51,23 @@ function saludar(firstName: string) {
 saludar('Leo')
 
 //Objets as functions -> One format
-function greeting({ name, age }: { name: string; age: number }) {
-  console.log(`Hello ${name}, you are ${age} years old`)
+function greeting({ name, power }: { name: string; power: number }) {
+  console.log(`Hello ${name}, you are ${power} years old`)
 }
 
-greeting({ name: 'Leo', age: 2 })
+greeting({ name: 'Leo', power: 2 })
 
 //Objets as functions -> Second format
-function greetingTwo(persona: { name: string; age: number }) {
-  const { name, age } = persona
-  console.log(`Hello ${name}, you are ${age} years old`)
+function greetingTwo(persona: { name: string; power: number }) {
+  const { name, power } = persona
+  console.log(`Hello ${name}, you are ${power} years old`)
 }
 
-greetingTwo({ name: 'Leo', age: 2 })
+greetingTwo({ name: 'Leo', power: 2 })
 
 //Functions that return
-function greetingThree({ name, age }: { name: string; age: number }): string {
-  console.log(`Hello ${name}, you are ${age} years old`)
+function greetingThree({ name, power }: { name: string; power: number }): string {
+  console.log(`Hello ${name}, you are ${power} years old`)
   //Inferency of return type
   return name
 }
@@ -81,3 +83,101 @@ const sayHi = (name: string) => {
 }
 
 sayHiFromFunction(sayHi)
+
+// Type arrow functions -> One format
+const sumar = (a: number, b: number): number => {
+  return a + b
+}
+
+//Second format
+const restar: (a: number, b: number) => number = (a, b) => {
+  return a - b
+}
+
+// Type never and void
+
+//Never -> When the function never return a value
+function throwError(message: string): never {
+  if (message) throw new Error(message)
+  throw new Error(message)
+}
+
+//Void -> When the function doesn't return anything
+function logMessage(message: string): void {
+  console.log(message)
+  // throw new Error(message);
+  // ...
+  //return implicitly <-- DA IGUAL -> Void -> No devuelve nada
+}
+
+//Inferencia con las funciones anónimas según el contexto
+const avengers = ['Spiderman', 'Hulk', 'Iron Man']
+
+avengers.forEach(avenger => {
+  console.log(avenger.toUpperCase)
+})
+
+avengers.map(function (avenger) {
+  console.log(avenger.toUpperCase())
+})
+
+//OBJETOS -> Inferencia de datos
+
+//Type Alias -> TIPOS PROPIOS (PascalCase)
+
+type Hero = {
+  name: string
+  power: number
+}
+
+let hero: Hero = {
+  name: 'Thor',
+  power: 1500,
+}
+
+function createHero(hero: Hero): Hero {
+  const { name, power } = hero
+  return { name, power }
+}
+
+const thor = createHero({ name: 'Thor', power: 1500 })
+
+// OPTIONAL PROPERTIES -> Propiedades opcionales -> ?
+// readonly -> Propiedad de lectura
+
+// Templates union types
+
+type HeroId = `${string}-${string}-${string}-${string}-${string}`
+
+type SecondHero = {
+  readonly id?: HeroId
+  name: string
+  power: number
+  isActive?: boolean
+}
+
+let secondHero: SecondHero = {
+  name: 'Thor',
+  power: 1500,
+}
+
+function secondCreateHero(hero: SecondHero): SecondHero {
+  const { name, power } = hero
+  return {
+    id: crypto.randomUUID(),
+    name,
+    power,
+    isActive: true,
+  }
+}
+
+//Object.freeze() -> Bloquea los cambios en el objeto -> Inmutable
+const secondThor = Object.freeze(secondCreateHero({ name: 'Thor', power: 1500 }))
+
+console.log(secondThor.isActive) // true
+
+// secondThor.id?.toString()
+
+//Mutabilidad -> ❌
+// secondThor.id = 42422434324324
+
